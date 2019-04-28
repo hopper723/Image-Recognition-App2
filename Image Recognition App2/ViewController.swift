a9e4a40faf0cd083ba72be9c95704a5433d4799e
hopper723 <hopper723@gmail.com>
2019-04-28 16:20:30 -0700

    Initial Commit

---
+++ Image Recognition App2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Image Recognition App2
+//
+//  Created by Hiu Man Yeung on 4/28/19.
+//  Copyright © 2019 Hiu Man Yeung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
