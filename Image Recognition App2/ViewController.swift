7a4f39a5131edf814c59e82264fe860717131a69
hopper723 <hopper723@gmail.com>
2019-04-29 11:18:45 -0700

    Add camera functionality and use pertained model from IBM Blumix for image recognition

--- Image Recognition App2/ViewController.swift
+++ Image Recognition App2/ViewController.swift
@@ -7,14 +7,48 @@
 //
 
 import UIKit
+import VisualRecognitionV3
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    let apiKey = API().visual_recognition_api
+    let version = "2019-04-28"
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
+        
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
     }
 
-
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = pickedImage
+            imagePicker.dismiss(animated: true, completion: nil)
+            
+            let visualRecognition = VisualRecognition(version: version, apiKey: apiKey)
+            visualRecognition.classify(image: pickedImage) { (response, error) in
+                if let result = response?.result {
+                    DispatchQueue.main.async {
+                        self.navigationItem.title = result.images.first!.classifiers.first!.classes.first!.className
+                    }
+                } else {
+                    print("Unable to classify image")
+                }
+            }
+        } else {
+            print("No photos")
+        }
+    }
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
 }
 
